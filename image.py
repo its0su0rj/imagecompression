5ccb139a4fe9e5142d779fa9bfa468655686264f
its0su0rj <155565040+its0su0rj@users.noreply.github.com>
2024-01-15 19:16:22 +0530

    Update image.py

--- image.py
+++ image.py
@@ -1,7 +1,8 @@
-import streamlit as st
 import numpy as np
+from sklearn.cluster import KMeans
 from PIL import Image
 import joblib
+import streamlit as st
 
 # Load the trained model
 model = joblib.load('image.joblib')
@@ -16,11 +17,16 @@ if uploaded_file is not None:
     # Load and preprocess the image
     image = Image.open(uploaded_file)
     image = np.array(image)
+
+    # Reshape the image to be a list of RGB values
     pixels = image.reshape(-1, 3)
 
     # Make predictions on the input data
-    compressed_pixels = model.cluster_centers_[model.labels_]
+    compressed_pixels = model.cluster_centers_[model.predict(pixels)]
     compressed_image = compressed_pixels.reshape(image.shape)
+
+    # Normalize pixel values to [0, 255]
+    compressed_image = np.clip(compressed_image, 0, 255).astype(np.uint8)
 
     # Display the original and compressed images
     st.image(image, caption='Original Image', use_column_width=True)
